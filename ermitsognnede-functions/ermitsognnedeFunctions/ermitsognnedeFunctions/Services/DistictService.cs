7ad998f39fca8ecad1087d91e302158707266200
Mathias-Givskov <mgi>
2021-04-22 15:44:38 +0200

    Fix SSI data

--- ermitsognnede-functions/ermitsognnedeFunctions/ermitsognnedeFunctions/Services/DistictService.cs
+++ ermitsognnede-functions/ermitsognnedeFunctions/ermitsognnedeFunctions/Services/DistictService.cs
@@ -26,12 +26,12 @@ namespace ermitsognnedeFunctions.Services
                     DistrictCode = (int)row.GetCell(1)?.NumericCellValue,
                     District = row.GetCell(2)?.StringCellValue,
                     Municipality = row.GetCell(3)?.StringCellValue,
-                    DistrictPopulationCount = (int)row.GetCell(4)?.NumericCellValue,
-                    Incidence = (int)row.GetCell(5)?.NumericCellValue,
-                    NewInfectedCount = (int)row.GetCell(6)?.NumericCellValue,
-                    PositivePercentage = (double)(row.GetCell(7)?.NumericCellValue),
-                    IsClosed = row.GetCell(8)?.StringCellValue?.ToLower() == "nedlukket",
-                    StartOfLatestAutomaticShutdown = row.GetCell(9)?.CellType == CellType.String ? null : row.GetCell(9)?.DateCellValue
+                    DistrictPopulationCount = (int)row.GetCell(5)?.NumericCellValue,
+                    Incidence = (int)row.GetCell(6)?.NumericCellValue,
+                    NewInfectedCount = (int)row.GetCell(7)?.NumericCellValue,
+                    PositivePercentage = (double)(row.GetCell(8)?.NumericCellValue),
+                    IsClosed = row.GetCell(9)?.StringCellValue?.ToLower() == "nedlukket",
+                    StartOfLatestAutomaticShutdown = row.GetCell(10)?.CellType == CellType.String ? null : row.GetCell(10)?.DateCellValue
                 });
             }
 
