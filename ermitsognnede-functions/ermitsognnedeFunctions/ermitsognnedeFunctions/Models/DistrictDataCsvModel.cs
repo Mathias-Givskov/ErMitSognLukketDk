67fdff46c962c798c256a9f9c4ce482a160d76ca
Mathias Givskov <mathias.givskov1@gmail.com>
2021-05-12 19:31:07 +0200

    Add csv file support

---
+++ ermitsognnede-functions/ermitsognnedeFunctions/ermitsognnedeFunctions/Models/DistrictDataCsvModel.cs
@@ -0,0 +1,36 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+
+namespace ermitsognnedeFunctions.Models
+{
+    public class DistrictDataCsvModel
+    {
+
+        [Name("Sognekode")]
+        public int DistrictCode { get; set; }
+
+        [Name("Sogn")]
+        public string District { get; set; }
+
+        [Name("Kommune1")]
+        public string Municipality { get; set; }
+
+        [Name("Indbyggertal i sogn")]
+        public int DistrictPopulationCount { get; set; }
+
+        [Index(9)]
+        public double Incidence { get; set; }
+
+        [Index(10)]
+        public int NewInfectedCount { get; set; }
+
+        [Index(11)]
+        public double PositivePercentage { get; set; }
+
+        [Name("Status for automatisk nedlukning")]
+        public string StatusForAutomaticClosing { get; set; }
+
+        [Name("Påbegyndelsesdato for seneste automatiske nedlukning af sognet")]
+        public string StartOfLatestAutomaticShutdown { get; set; }
+    }
+}
--- ermitsognnede-functions/ermitsognnedeFunctions/ermitsognnedeFunctions/Services/Interfaces/IDistictService.cs
+++ ermitsognnede-functions/ermitsognnedeFunctions/ermitsognnedeFunctions/Services/Interfaces/IDistictService.cs
@@ -1,11 +1,10 @@
 ﻿using ermitsognnedeFunctions.Models;
 using System.Collections.Generic;
-using System.IO;
 
 namespace ermitsognnedeFunctions.Services.Interfaces
 {
     public interface IDistictService
     {
-        List<DistrictDataJsonModel> GetDistrictDataJsonModels(Stream excelFileStream);
+        List<DistrictDataJsonModel> GetDistrictDataJsonModels(FileModel fileModel);
     }
 }
